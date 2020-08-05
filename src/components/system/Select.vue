3c24c1927a052695b74412358727560048544bf0
Austin Fay <amftech512@gmail.com>
2020-08-05 12:16:48 -0600

    merged changes from SillyCTF; added TaxSelect component

---
+++ src/components/system/Select.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="text-comp">
+    <v-card class="card">
+      <v-card-title>
+        <h2>{{ props.headerTxt }}</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-select
+          v-model="retVal.content"
+          :items="props.options"
+          item-text="text"
+          item-value="value"
+          filled
+          label="Select"
+        ></v-select>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectComponent',
+  props: {
+    name: String,
+    props: {
+      type: Object,
+      default() {
+        return {
+          headerTxt: 'Select',
+          options: {}
+        };
+      }
+    },
+    value: {
+      type: Object,
+      default() {
+        return {
+          content: ''
+        };
+      }
+    }
+  },
+  data() {
+    return {
+      retVal: this.value
+    };
+  },
+  created() {
+    this.updateData();
+  },
+  methods: {
+    updateData() {
+      this.$emit('input', this.retVal);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.text-comp {
+  margin: 10px;
+
+  .card {
+    // min-width: 400px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    line-height: 2.5rem;
+  }
+}
+</style>
--- src/pages/RegularPageIndex.vue
+++ src/pages/RegularPageIndex.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="reg-index-page">
-    <div v-if="loading" class="loading">    
+    <div v-if="loading" class="loading">
       <v-progress-circular
         :size="70"
         :width="7"
@@ -8,23 +8,34 @@
         indeterminate
       ></v-progress-circular>
     </div>
-    <div class="card-container" v-else>
-      <v-card class="card" v-for="page in Pages" :key="page.id">
-        <v-card-title>
-          <router-link :to="`/reg/${$route.params.index}/${page.id}`">{{ getTitle(page) }}</router-link>
+    <div v-else class="card-container">
+      <v-card v-for="page in Pages" :key="page.id" class="card">
+        <v-card-title class="title">
+          <router-link :to="`/reg/${$route.params.index}/${page.id}`">{{
+            getTitle(page)
+          }}</router-link>
+          <v-btn
+            class="align-right"
+            icon
+            @click="delPage(page.id, getTitle(page))"
+          >
+            <v-icon color="grey lighten-1">mdi-delete</v-icon>
+          </v-btn>
         </v-card-title>
       </v-card>
     </div>
     <div class="add-btn-div">
-      <v-btn fab color="primary" @click="addPage"><v-icon dark>mdi-plus</v-icon></v-btn>
+      <v-btn fab color="primary" @click="addPage"
+        ><v-icon dark>mdi-plus</v-icon></v-btn
+      >
     </div>
   </div>
 </template>
 
 <script>
-import database from '@/firebase/firestore.js'
-import { RegularPages } from '@/pages/index.js'
-import { getProp } from '@/lib/nested-obj.js'
+import database from '@/firebase/firestore.js';
+import { RegularPages } from '@/pages/index.js';
+import { getProp } from '@/lib/nested-obj.js';
 
 export default {
   name: 'RegularPage',
@@ -33,41 +44,52 @@ export default {
       RegularPages,
       Pages: [],
       loading: true
-    }
+    };
   },
   computed: {
     Page() {
       return RegularPages[this.$route.params.index];
     },
     collectionRef() {
-      let path = ((this.Page.dbPath[0] == '/')? '' : '/') + this.Page.dbPath;
-      let docRef = database.collection(path);
+      const path = (this.Page.dbPath[0] === '/' ? '' : '/') + this.Page.dbPath;
+      const docRef = database.collection(path);
       return docRef;
     }
   },
+  async created() {
+    (await this.collectionRef.get()).forEach((doc) => this.Pages.push(doc));
+    this.loading = false;
+  },
+  updated() {
+    this.$store.commit('setPageTitle', this.Page.name);
+  },
   methods: {
     getTitle(doc) {
-      if(!this.Page.listItem)
+      if (!this.Page.listItem) {
         return doc.id;
+      }
 
       return getProp(doc.data(), this.Page.listItem.title) || doc.id;
     },
     addPage() {
       this.$router.push(`${this.$route.path}/$new`);
+    },
+    async delPage(id, name) {
+      if (confirm(`Are you sure you want to delete ${name}?`)) {
+        try {
+          await this.collectionRef.doc(id).delete();
+          this.Pages = this.Pages.filter((page) => page.id !== id);
+        } catch (e) {
+          console.error(e);
+          alert('Oops! An error has occurred.');
+        }
+      }
     }
-  },
-  async created() {
-    (await this.collectionRef.get()).forEach(doc => this.Pages.push(doc));
-    this.loading = false;
-  },
-  updated() {
-    this.$store.commit('setPageTitle', this.Page.name);
   }
-}
+};
 </script>
 
 <style lang="scss">
-
 .reg-index-page {
   .loading {
     text-align: center;
@@ -82,11 +104,14 @@ export default {
   .card {
     // min-width: 400px;
     margin: 10px auto;
+
+    > .title {
+      justify-content: space-between;
+    }
   }
 
   .card-container {
     padding: 10px;
   }
 }
-
 </style>
